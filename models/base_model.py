8d5238f002b93af6233d2aeee60506045f7a6568
IyasuH <eyasuhailegbr@gmail.com>
2022-01-25 09:00:48 -0800

    task 4 added

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 """import modules"""
 from datetime import datetime
+import json
 import uuid
 
 class BaseModel():
     """Baseclass"""
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """public instance attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                if key == 'created_at':
+                    value = datetime.fromisoformat(value)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         """__str__ method to return [<class name>] (<self.id>) <self.__dict__>"""
