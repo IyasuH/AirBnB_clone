7650be749b70eced42d61b63eb3a413d6d1f0169
IyasuH <eyasuhailegbr@gmail.com>
2022-01-25 11:38:57 -0800

    test file updated

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -2,6 +2,7 @@
 """
 import modules
 """
+from datetime import datetime
 import unittest
 from models.base_model import BaseModel
 """
@@ -16,12 +17,12 @@ class TestBase(unittest.TestCase):
         id_type = type(my_model.id)
         self.assertEqual(id_type, str)
 
-    def to_dict(self):
+    def test_dict(self):
         """Test for instance to dictionary"""
         my_model = BaseModel()
         my_model.name = "My first Model"
         my_model.my_number = 89
-        my_model_json = b.to_dict()
+        my_model_json = my_model.to_dict()
         self.assertIn('my_number', my_model_json)
         self.assertIn('name', my_model_json)
         self.assertIn('__class__', my_model_json)
@@ -29,6 +30,25 @@ class TestBase(unittest.TestCase):
         self.assertIn('id', my_model_json)
         self.assertIn('created_at', my_model_json)
         """Test for Base Model"""
+
+    def test_baseModel_dict(self):
+        """Test the re-crated instsnace with the dictionary"""
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        my_new_model = BaseModel(**my_model_json)
+        self.assertEqual(my_model.id, my_new_model.id)
+        self.assertNotIn('__class__', my_new_model.__dict__)
+        self.assertIn('name', my_new_model.__dict__)
+        self.assertIn('id', my_new_model.__dict__)
+        self.assertIn('my_number', my_new_model.__dict__)
+        self.assertIn('updated_at', my_new_model.__dict__)
+        self.assertIn('created_at', my_new_model.__dict__)
+        created_type = type(my_new_model.created_at)
+        self.assertEqual(created_type, datetime)
+        updated_type = type(my_new_model.updated_at)
+        self.assertEqual(updated_type, datetime)
 
 if __name__ == '__main__':
     unittest.main()
