adaace1aaed04c1112847eaed532b53e5a8a8cf7
IyasuH <eyasuhailegbr@gmail.com>
2022-01-26 12:05:29 -0800

    update and fix errors

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,11 @@ class BaseModel():
                     value = datetime.fromisoformat(value)
                 if key != '__class__':
                     setattr(self, key, value)
-
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """__str__ method to return [<class name>] (<self.id>) <self.__dict__>"""
@@ -30,13 +34,14 @@ class BaseModel():
     def save(self):
         """update the public instance attribute
         updated_at with the current datetime"""
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
-        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
-        self.__dict__['updated_at'] = datetime.now().isoformat()
-        self.__dict__['created_at'] = datetime.now().isoformat()
-        self.__dict__['__class__'] = self.__class__.__name__
-        return (self.__dict__)
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['updated_at'] = datetime.now().isoformat()
+        dict_copy['created_at'] = datetime.now().isoformat()
+        return (dict_copy)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,19 +17,24 @@ class FileStorage():
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         obj_key = type(obj).__name__ + '.' + obj.id
-        FileStorage.__objects[obj_key] = obj.to_dict()
+        FileStorage.__objects[obj_key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (__file_path)"""
-        with open(FileStorage.__file_path, "w") as x1:
-            json.dump(FileStorage.__objects, x1)
+        json_dict={}
+        for k, v in FileStorage.__objects.items():
+            json_dict[k] = v.to_dict()
+        with open(FileStorage.__file_path, mode="w", encoding='utf-8') as x:
+            x.write(json.dumps(json_dict))
 
     def reload(self):
         """deserializes the JSON file to __objects only if
-        the JSON file exists: otherise, do nothing
+        the JSON file exists: otherwise, do nothing
         no exception should be raised"""
         if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, mode='r') as x:
-                json.load(x)
+            with open(FileStorage.__file_path, mode="r", encoding='utf-8') as x:
+                dict_back = json.loads(x.read())
+                for k, v in dict_back.items():
+                    FileStorage.__objects[k] = eval(v['__class__'])(**v) 
         else:
             pass
