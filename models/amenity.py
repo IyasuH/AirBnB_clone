caa827471bf28e709c678d684684e91c1cf357ea
IyasuH <eyasuhailegbr@gmail.com>
2022-01-28 14:23:00 -0800

    task 9

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+import modules
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """class for Amenity"""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+import modules
+"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """class for reviews"""
+    place_id = ""
+    user_id = ""
+    text = ""
