7cf74745d0c4df45eca3c690de499e05ff0ef1cc
IyasuH <eyasuhailegbr@gmail.com>
2022-01-29 05:41:04 -0800

    update

--- models/base_model.py
+++ models/base_model.py
@@ -11,10 +11,7 @@ class BaseModel():
     """Baseclass"""
     def __init__(self, *args, **kwargs):
         """public instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs is not None:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == 'updated_at':
                     value = datetime.fromisoformat(value)
