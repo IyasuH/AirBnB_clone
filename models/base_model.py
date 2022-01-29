3bef0b6193f392de90f454c69758c8415dfdfbd3
IyasuH <eyasuhailegbr@gmail.com>
2022-01-29 14:27:05 -0800

    update

--- models/base_model.py
+++ models/base_model.py
@@ -12,15 +12,12 @@ class BaseModel():
         """public instance attributes"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == "id":
-                    self.id = kwargs[key]
-                elif key == 'updated_at':
-                    self.updated_at = datetime.fromisoformat(value)
-                elif key == 'created_at':
-                    self.created_at = datetime.fromisoformat(value)
-                else:
-                    if key != '__class__':
-                        setattr(self, key, value)
+                if key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'created_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -42,9 +39,8 @@ class BaseModel():
     def to_dict(self):
         """Returns a dictionary containing all
         keys/values of __dict__ of the instance"""
-        dict_copy = {}
+        dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = self.__class__.__name__
-        dict_copy.update(self.__dict__)
-        dict_copy.update({'created_at': dict_copy['created_at'].isoformat()})
-        dict_copy.update({'updated_at': dict_copy['updated_at'].isoformat()})
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        dict_copy['created_at'] = self.created_at.isoformat()
         return dict_copy
